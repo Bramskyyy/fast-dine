ea63e5c52fe902494e36626f9a6e781c683783ff
Kevin Bourgonjon <kevin.bourgonjon@student.odisee.be>
2016-11-30 22:48:12 +0100

    WERKT NIET... ALLES IS VOOR NIKS... TIME WASTE... SCRUM PROJECT FAILED... UITSCHRIJVEN GRAAG... VOLGENDE KEER BETER...

--- Code/Server/fastdine/src/main/java/fastdine/UserController.java
+++ Code/Server/fastdine/src/main/java/fastdine/UserController.java
@@ -3,6 +3,7 @@ package fastdine;
 import dataEntities.User;
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,13 +19,20 @@ public class UserController {
     // Returns the hashed password for a user based on its email
     @RequestMapping("/userPassword")
     public String getUserPasswordByEmail (@RequestParam(value="email") String userEmail) {
-          
-        sql = "SELECT password FROM users WHERE email= ? LIMIT 1";
+        if (!userEmail.isEmpty()) {
+            sql = "SELECT password FROM users WHERE email= ? LIMIT 1";
+            
+            try 
+            {
+                String result = jdbcTemplate.queryForObject(sql, new Object[] { userEmail }, String.class);
+                if (!result.equals("")) return result;
+            }
+            catch (Exception e)
+            {
+                return "Exception: " + e.getMessage();
+            }
+        }
         
-        String result = jdbcTemplate.queryForObject(sql, new Object[] { userEmail }, String.class);
-        
-        if (!result.equals("")) return result;
-               
         return null;
     }
 }
---
+++ Code/Server/fastdine/src/test/java/ControllerTests.java
@@ -0,0 +1,38 @@
+
+import fastdine.*;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+//WERKT NIET... ALLES IS VOOR NIKS... TIME WASTE... SCRUM PROJECT FAILED... UITSCHRIJVEN GRAAG... VOLGENDE KEER BETER...
+
+public class ControllerTests {
+    
+//    private UserController uc;
+//    
+//    @Before
+//    public void setUp ()
+//    {
+//        uc = new UserController();
+//    }    
+//    
+//    @Test
+//    public void testGetUserPasswordByEmailEmptyString ()
+//    {
+//        String emptyVal = uc.getUserPasswordByEmail("");
+//        assertEquals(null, emptyVal);
+//    }
+//    
+//    @Test
+//    public void testGetUserPasswordByEmailRandomString ()
+//    {
+//        String randomVal = uc.getUserPasswordByEmail("azertyuiopqsdfghjklmwxcvbn");
+//        assertEquals(null, randomVal);
+//    }
+//    
+//    @Test
+//    public void testGetUserPasswordByEmailExistingString () {
+//        String existingVal = uc.getUserPasswordByEmail("gebruiker1@odisee.be");
+//        assertEquals("Azerty1", existingVal);
+//    }
+}

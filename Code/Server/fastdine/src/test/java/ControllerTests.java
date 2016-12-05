ff1d6cc926e9f19154eb286ad89835e686ed4d52
Kevin Bourgonjon <kevin.bourgonjon@student.odisee.be>
2016-12-05 03:11:27 +0100

    some progress not much

--- Code/Server/fastdine/src/test/java/ControllerTests.java
+++ Code/Server/fastdine/src/test/java/ControllerTests.java
@@ -1,6 +1,4 @@
-
 import dataEntities.*;
-import fastdine.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,41 +14,228 @@ import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import static org.junit.Assert.assertEquals;
-import org.junit.Before;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
-import sun.net.www.http.HttpClient;
 
-public class ControllerTests {
-    private String getValueFromURL(String urlRequest)
+public class ControllerTests {    
+    /* ReservationController Tests
+    getReservationsByRestaurantId - TODO check id=0
+    getReservationsByRestaurantIdAndShift - TODO check id=0
+    newReservation - TODO
+    */
+    @Test
+    public void getReservationsByRestaurantId () throws Exception
     {
+        String id = "1";
+        
+        String valueAPI = getValueFromURL("http://localhost:8080/reservations?id=" + id);
+        String valueSQL = "[";
+        
         try {
-            URL url = new URL(urlRequest);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Accept", "application/json");
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection con = DriverManager.getConnection("jdbc:mysql://10.129.32.188/fastdine", "root", "Azerty123");
 
-            if (conn.getResponseCode() != 200) {
-                    throw new RuntimeException("Failed : HTTP error code : "
-                                    + conn.getResponseCode());
+            Statement st = con.createStatement();
+            String sql = ("select users.name, users.telephone, reservations.date, reservations.shift, tables.tablenumber from users " +
+                "inner join reservations on users.id = reservations.user_id " +
+                "inner join tables_has_reservations on reservations.id = tables_has_reservations.reservation_id " +
+                "inner join tables on tables_has_reservations.table_id = tables.id " +
+                "inner join restaurants on tables.restaurant_id = restaurants.id " +
+                "WHERE restaurants.id = '" + id + "';");
+            ResultSet rs = st.executeQuery(sql);
+           
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String telephone = rs.getString("telephone");
+                String date  = rs.getDate("date").toString();
+                String shift  = Integer.toString(rs.getInt("shift"));
+                String tablenumber = Integer.toString(rs.getInt("tablenumber"));
+                
+                valueSQL +=  "\"" + name + "\",\"" + telephone + "\",\"" + date + "\",\"" + shift + "\",\"" + tablenumber + "\",";
             }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
-             
-            String output = br.readLine();
-
-            conn.disconnect();
-            return output;
             
-        } catch (MalformedURLException e) {
-              e.printStackTrace();
-        } catch (IOException e) {
-              e.printStackTrace();
-        }       
-       
-        return null;
+            valueSQL = valueSQL.substring(0,valueSQL.length() - 1);
+            valueSQL += "]";
+            valueSQL = valueSQL.replaceAll("\"null\"", "null");
+
+            con.close();
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        
+        assertEquals(valueSQL, valueAPI);
     }
     
+    @Test
+    public void getReservationsByRestaurantIdAndShiftTest () throws Exception
+    {
+        String id = "1";
+        String shift = "3";
+        
+        String valueAPI = getValueFromURL("http://localhost:8080/reservations?id=" + id + "&shift=" + shift);
+        String valueSQL = "[";
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection con = DriverManager.getConnection("jdbc:mysql://10.129.32.188/fastdine", "root", "Azerty123");
+
+            Statement st = con.createStatement();
+            String sql = ("select users.name, users.telephone, reservations.date, tables.tablenumber from users " +
+                "inner join reservations on users.id = reservations.user_id " +
+                "inner join tables_has_reservations on reservations.id = tables_has_reservations.reservation_id " +
+                "inner join tables on tables_has_reservations.table_id = tables.id " +
+                "inner join restaurants on tables.restaurant_id = restaurants.id " +
+                "WHERE restaurants.id = " + id +
+                " AND reservations.shift = " + shift + ";");
+            ResultSet rs = st.executeQuery(sql);
+           
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String telephone = rs.getString("telephone");
+                String date  = rs.getDate("date").toString();
+                String tablenumber = Integer.toString(rs.getInt("tablenumber"));
+                valueSQL +=  "\"" + name + "\",\"" + telephone + "\",\"" + date + "\",\"" + tablenumber + "\",";
+            }
+            
+            valueSQL = valueSQL.substring(0,valueSQL.length() - 1);
+            valueSQL += "]";
+            valueSQL = valueSQL.replaceAll("\"null\"", "null");
+            
+            con.close();
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        
+        assertEquals(valueSQL, valueAPI);
+    }
+    
+    @Test
+    public void newReservationTest () throws Exception
+    {
+        assertEquals("TRUE","TRUE");
+    }
+    
+    /* RestaurantController Tests
+    getRestaurantByName - done
+    getAllRestaurants - done
+    */
+    @Test
+    public void getRestaurantByNameTest () throws Exception
+    {
+        List<Restaurant> restosAPI = new ArrayList<Restaurant>();
+        List<Restaurant> restosSQL = new ArrayList<Restaurant>();
+        
+        String restaurantName = "de";
+      
+        String value = getValueFromURL("http://localhost:8080/restaurant?name=" + restaurantName);
+
+        JSONArray mJsonArray = new JSONArray(value);
+        JSONObject mJsonObject = new JSONObject();
+
+        for (int i = 0; i < mJsonArray.length(); i++) {
+            mJsonObject = mJsonArray.getJSONObject(i);
+
+            int id = mJsonObject.getInt("id");
+            String name = mJsonObject.getString("name");
+            String location = mJsonObject.getString("location");
+            String email = mJsonObject.getString("email");
+            String telephone = mJsonObject.getString("telephone");
+            int seats = mJsonObject.getInt("seats");
+
+            restosAPI.add(new Restaurant(id, name, location, email, telephone, seats));
+        };
+                     
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection con = DriverManager.getConnection("jdbc:mysql://10.129.32.188/fastdine", "root", "Azerty123");
+
+            Statement st = con.createStatement();
+            String sql = ("SELECT * FROM restaurants WHERE name LIKE '%" + restaurantName + "%'");
+            ResultSet rs = st.executeQuery(sql);
+           
+            while (rs.next()) { 
+                int id = rs.getInt("id"); 
+                String name = rs.getString("name");
+                String location = rs.getString("location");
+                String email = rs.getString("email");
+                String telephone = rs.getString("telephone");
+                int seats = rs.getInt("seats");
+                
+                restosSQL.add(new Restaurant(id, name, location, email, telephone, seats));
+            }
+
+            con.close();
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        
+        assertEquals(restosSQL.size(), restosAPI.size());
+        
+        int x = 0;
+        for (Restaurant r : restosSQL) assertEquals(r.toString(), restosAPI.get(x++).toString());
+    }
+    
+    @Test
+    public void getAllRestaurantsTest () throws Exception
+    {
+        List<Restaurant> restosAPI = new ArrayList<Restaurant>();
+        List<Restaurant> restosSQL = new ArrayList<Restaurant>();
+      
+        String value = getValueFromURL("http://localhost:8080/restaurants");
+
+        JSONArray mJsonArray = new JSONArray(value);
+        JSONObject mJsonObject = new JSONObject();
+
+        for (int i = 0; i < mJsonArray.length(); i++) {
+            mJsonObject = mJsonArray.getJSONObject(i);
+
+            int id = mJsonObject.getInt("id");
+            String name = mJsonObject.getString("name");
+            String location = mJsonObject.getString("location");
+            String email = mJsonObject.getString("email");
+            String telephone = mJsonObject.getString("telephone");
+            int seats = mJsonObject.getInt("seats");
+
+            restosAPI.add(new Restaurant(id, name, location, email, telephone, seats));
+        };
+                     
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection con = DriverManager.getConnection("jdbc:mysql://10.129.32.188/fastdine", "root", "Azerty123");
+
+            Statement st = con.createStatement();
+            String sql = ("SELECT * FROM restaurants;");
+            ResultSet rs = st.executeQuery(sql);
+           
+            while (rs.next()) { 
+                int id = rs.getInt("id"); 
+                String name = rs.getString("name");
+                String location = rs.getString("location");
+                String email = rs.getString("email");
+                String telephone = rs.getString("telephone");
+                int seats = rs.getInt("seats");
+                
+                restosSQL.add(new Restaurant(id, name, location, email, telephone, seats));
+            }
+
+            con.close(); 
+        }
+        catch (Exception e) {
+            throw e;
+        }
+        
+        assertEquals(restosSQL.size(), restosAPI.size());
+        
+        int x = 0;
+        for (Restaurant r : restosSQL) assertEquals(r.toString(), restosAPI.get(x++).toString());
+    }
+    
+    /* TableController Tests
+    getTablesByRestaurantIdAndShiftAndDate - done
+    */
     @Test
     public void getTablesByRestaurantIdAndShiftAndDateTest() throws Exception {
         List<Table> tablesAPI = new ArrayList<Table>();
@@ -109,9 +294,12 @@ public class ControllerTests {
         
         int x = 0;
         for (Table i : tablesSQL) assertEquals(i.toString(), tablesAPI.get(x++).toString());  
-       
     }
     
+    /* UserController Tests
+    getUserPasswordByEmail - done
+    newUser - TODO
+    */
     @Test
     public void getUserPasswordByEmailTest() throws Exception {
         String passwordAPI = "0";
@@ -144,98 +332,42 @@ public class ControllerTests {
     }
     
     @Test
-    public void getAllRestaurantsTest () throws Exception
+    public void newUserTest() throws Exception {
+        assertEquals("TRUE","TRUE");
+    }
+    
+    // Extra methods
+    private String getValueFromURL(String urlRequest)
     {
-        List<Restaurant> restosAPI = new ArrayList<Restaurant>();
-        List<Restaurant> restosSQL = new ArrayList<Restaurant>();
-      
-        String value = getValueFromURL("http://localhost:8080/restaurants");
-
-        JSONArray mJsonArray = new JSONArray(value);
-        JSONObject mJsonObject = new JSONObject();
-
-        for (int i = 0; i < mJsonArray.length(); i++) {
-            mJsonObject = mJsonArray.getJSONObject(i);
-
-            int id = mJsonObject.getInt("id");
-            String name = mJsonObject.getString("name");
-            String location = mJsonObject.getString("location");
-            String email = mJsonObject.getString("email");
-            String telephone = mJsonObject.getString("telephone");
-            int seats = mJsonObject.getInt("seats");
-
-            restosAPI.add(new Restaurant(id, name, location, email, telephone, seats));
-        };
-                     
         try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            Connection con = DriverManager.getConnection("jdbc:mysql://10.129.32.188/fastdine", "root", "Azerty123");
+            URL url = new URL(urlRequest);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
 
-            Statement st = con.createStatement();
-            String sql = ("SELECT * FROM restaurants;");
-            ResultSet rs = st.executeQuery(sql);
-
-           
-            while (rs.next()) { 
-                int id = rs.getInt("id"); 
-                String name = rs.getString("name");
-                String location = rs.getString("location");
-                String email = rs.getString("email");
-                String telephone = rs.getString("telephone");
-                int seats = rs.getInt("seats");
-                
-                restosSQL.add(new Restaurant(id, name, location, email, telephone, seats));
+            if (conn.getResponseCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : "
+                                    + conn.getResponseCode());
             }
 
-            con.close();
-                      
-        }
-        catch (Exception e) {
-            throw e;
-        }
-        
-        assertEquals(restosSQL.size(), restosAPI.size());
-        
-        int x = 0;
-        for (Restaurant r : restosSQL) assertEquals(r.toString(), restosAPI.get(x++).toString());        
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+             
+            String output = br.readLine();
 
+            conn.disconnect();
+            return output;
+            
+        } catch (MalformedURLException e) {
+              e.printStackTrace();
+        } catch (IOException e) {
+              e.printStackTrace();
+        }       
+        
+        return null;
     }
     
-    @Test
-    public void showData() {
-        String value = getValueFromURL("http://localhost:8080/tables?id=1&shift=1&date=2017-01-01");
+    private void showData() {
+        String value = getValueFromURL("http://localhost:8080/reservations?id=1&shift=3");
         System.out.println(value);
-        
-        value = getValueFromURL("http://localhost:8080/restaurants");
-        System.out.println(value);
-        
     }
-    
-//    private UserController uc;
-//    
-//    @Before
-//    public void setUp ()
-//    {
-//        uc = new UserController();
-//    }    
-//    
-//    @Test
-//    public void testGetUserPasswordByEmailEmptyString ()
-//    {
-//        String emptyVal = uc.getUserPasswordByEmail("");
-//        assertEquals(null, emptyVal);
-//    }
-//    
-//    @Test
-//    public void testGetUserPasswordByEmailRandomString ()
-//    {
-//        String randomVal = uc.getUserPasswordByEmail("azertyuiopqsdfghjklmwxcvbn");
-//        assertEquals(null, randomVal);
-//    }
-//    
-//    @Test
-//    public void testGetUserPasswordByEmailExistingString () {
-//        String existingVal = uc.getUserPasswordByEmail("gebruiker1@odisee.be");
-//        assertEquals("Azerty1", existingVal);
-//    }
 }
